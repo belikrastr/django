5c5ed43a7d0581aca20c512d95d309dfae147990
Беликов Владимир <belikrastr@yandex.ru>
2023-10-17 00:20:36 +0300

    update

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, \
+                                  PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts}
-                  )
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
